a2a5f438158cbe765d1ee3fca605a6e050bf697a
深渊の鸽子 <ghzero97@gmail.com>
2022-11-08 00:43:35 +0100

    修改

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/zerxz/zerxz-web',
+            'https://github.com/ZerxZ/zerxz-web/blob/master',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-              'https://github.com/zerxz/zerxz-web',
+              'https://github.com/ZerxZ/zerxz-web/blob/master',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
